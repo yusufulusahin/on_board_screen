39aaa4e6ccd6c6a1ff61c132db434cc886510951
Yusuf <yusufulusahinn@gmail.com>
2025-03-09 20:54:56 +0300

    topla

---
+++ android/app/src/main/kotlin/com/example/on_board_screen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.on_board_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/view/_autthanticate/onboard/viewmodel/onboard_viewmodel.dart
@@ -0,0 +1,6 @@
+import 'package:mobx/mobx.dart';
+part 'onboard_viewmodel.g.dart';
+
+class OnboardViewModel = _OnboardViewModelBase with _$OnboardViewModel;
+
+abstract class _OnboardViewModelBase with Store {}
